1db917b9040da5b79dee77ca02147074f9521da1
ROHITH NELLURI <46403142+rohith8888@users.noreply.github.com>
2019-04-28 23:29:06 +0530

    Add files via upload

---
+++ TCP_2W_sender.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.net.*;
+public class TCP_2W_sender
+{ 
+public static void main(String[] args) throws Exception
+{ 
+ServerSocket sersock = new ServerSocket(3000);    
+System.out.println("Server ready forchatting");   
+Socket sock = sersock.accept( );             
+ BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
+OutputStream ostream = sock.getOutputStream();
+ PrintWriter pwrite = new PrintWriter(ostream, true);
+ InputStream istream = sock.getInputStream();
+ BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
+ String receiveMessage, sendMessage;
+  while(true)
+{
+ if((receiveMessage = receiveRead.readLine()) != null)
+{
+System.out.println(receiveMessage);
+sendMessage = keyRead.readLine();
+pwrite.println(sendMessage); pwrite.flush(); 
+}
+}
+}
+}
